508854a09b3d6c74b81a8d0589f767fbb98a02f1
Tabitha Lee <tel1@andrew.cmu.edu>
2023-07-30 16:33:52 -0400

    Added version number

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 rl-utils
 """
 
+import io
+import os
+import re
 from setuptools import setup
 
 requirements = [
@@ -10,8 +13,14 @@ requirements = [
     'scipy'
 ]
 
+def package_version():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    version_file_path = os.path.join(current_dir, "rl_utils", "__init__.py")
+    with io.open(version_file_path, encoding="utf-8") as f:
+        return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
+
 setup(name='rl-utils',
-      version='0.0.0',
+      version=package_version(),
       description='A library for reinforcement learning algorithms, including REPS.',
       author='Tabitha Lee',
       author_email='tabithalee@cmu.edu',
